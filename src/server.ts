0ed3f3764daa8361fb136928dd51489378752ded
shariarmdimtiaz <shariar.md.imtiaz@gmail.com>
2024-05-22 13:37:58 +0900

    config added

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function main() {
+    try{
+        await mongoose.connect(config.dbUrl as string);
+    
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+        })
+    } catch (err) {
+        console.log(err);
+    }
+  }
+
+main();
+
+  
